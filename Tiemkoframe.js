e3fffc2279e1a2a8b84809f46f809bc4ae4dbc31
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-09-16 22:43:26 +0200

    Add files via upload

--- Tiemkoframe.js
+++ Tiemkoframe.js
@@ -9,6 +9,6 @@ linkURL = "index.htm"
 Tiemkoframe = new Array ("accueil.htm", "Ce qui permit Tiemko accès.htm", "Tiemko 1986-1995 accès.htm,  "Tiemko post Tiemko accès.htm", "Éric accès.htm", "Jean-Jacques accès.htm",","Rémy accès.htm")
 		
 function newIframe(ach) {
-	content.document.location.href = Magonframe[ach]
+	content.document.location.href = Tiemkoframe[ach]
 
 }
