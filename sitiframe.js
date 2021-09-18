49827d5f61e46d79cf3850781686e71e77e33909
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-09-18 18:22:32 +0200

    Add files via upload

--- sitiframe.js
+++ sitiframe.js
@@ -6,9 +6,9 @@ linkURL = "index.htm"
 		}
 	}
 	
-Tiemkoframe = new Array ("accueil.htm", "avant.htm", "Tiemko1986-1995.htm,  "TiemkopostTiemko.htm", "ed.htm", "jjt.htm", "rc.htm")
+Magonframe = new Array ("accueil.htm", "forumacces.htm","oeuvresacces.htm","liensacces.htm","avant.htm", "Tiemko1986-1995.htm,  "TiemkopostTiemko.htm", "ed.htm", "jjt.htm",","rc.htm")
 		
 function newIframe(ach) {
-	content.document.location.href = Tiemkoframe[ach]
+	content.document.location.href = Magonframe[ach]
 
 }
