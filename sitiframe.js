d3e359998c97ee0a1b291250cda3f16f02939c6b
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-09-26 00:11:37 +0200

    Update sitiframe.js

--- sitiframe.js
+++ sitiframe.js
@@ -6,7 +6,7 @@ linkURL = "index.htm"
 		}
 	}
 	
-Tiemkoframe = new Array ("accueil.htm", "accueil.htm","avant.htm", "1986-1995.htm", "après1995.htm","jjt.htm","ed.htm","rc.htm","liens.htm")
+Tiemkoframe = new Array ("accueil.htm", "accueil.htm","avant.htm", "1986-1995.htm", "après1995.htm","ed.htm","jjt.htm","rc.htm","liens.htm")
 		
 function newIframe(ach) {
 	content.document.location.href = Tiemkoframe[ach]
