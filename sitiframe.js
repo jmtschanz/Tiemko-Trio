d0b879b59c255aab39b3e577444893f5dad1f465
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-08 02:49:05 +0200

    Update sitiframe.js

--- sitiframe.js
+++ sitiframe.js
@@ -13,3 +13,10 @@ function newIframe(ach) {
 
 }
 
+function set_body_height() { // set body height = window height
+        $('body').height($(window).height());
+    }
+    $(document).ready(function() {
+        $(window).bind('resize', set_body_height);
+        set_body_height();
+    }
