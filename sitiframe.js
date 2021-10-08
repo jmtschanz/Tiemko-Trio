c7fdd6fb1d5b5509f64d557e00d1897c3283296a
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-08 12:26:28 +0200

    Update sitiframe.js

--- sitiframe.js
+++ sitiframe.js
@@ -5,7 +5,15 @@ linkURL = "index.htm"
 			linkURL = callingURL.substring(callingURL.indexOf('?')+1,callingURL.length)
 		}
 	}
-	
+
+function set_body_height() { // set body height = window height
+        $('body').height($(window).height());
+    }
+    $(document).ready(function() {
+        $(window).bind('resize', set_body_height);
+        set_body_height();
+    }
+
 Tiemkoframe = new Array ("accueil.htm", "accueil.htm","avant.htm", "1986-1995.htm", "après1995.htm","ed.htm","jjt.htm","rc.htm","Deboco.htm")
 		
 function newIframe(ach) {
