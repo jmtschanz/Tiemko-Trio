f96c093623417b00de387df523c52f6cd477654e
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-08 23:16:32 +0200

    Update sitiframe.js

--- sitiframe.js
+++ sitiframe.js
@@ -26,3 +26,26 @@ function mouseaway(avt)
 		document.write.src = ChgImg[avt]
 	
 		}
+
+if (document.images) {
+		Accl = new Image
+		Avnt = new Image
+		1986-95 = new Image
+		Apr = new Image
+
+		Accl.src = "Accueil.png"
+		Avnt.src = "Avant.png"
+		1986-95.src = "1986-1995.png"
+		Apr.src = "Après1995.png"
+	
+		Acclx = new Image
+		Avntx = new Image
+		1986-95x = new Image
+		Aprx = new Image
+
+		Acclx.src = "Accueilx.png"
+		Avntx.src = "Avantx.png"
+		1986-95x.src = "1986-1995x.png"
+		Aprx.src = "Après1995x.png"
+
+	}
