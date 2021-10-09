70398fd9ea6cc5dbd3710d1d46af7b87f37dd1c4
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-09 21:44:15 +0200

    Update sitiframe.js

--- sitiframe.js
+++ sitiframe.js
@@ -34,3 +34,11 @@ if (document.images) {
 		Aprx.src = "Après1995x.png"
 
 	}
+
+function set_body_height() { // set body height = window height
+        $('body').height($(window).height());
+    }
+    $(document).ready(function() {
+        $(window).bind('resize', set_body_height);
+        set_body_height();
+    }
