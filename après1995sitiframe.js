744265c2bb6513b0fb659a5c3b7cbc8d0d7e3a75
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-16 23:38:05 +0200

    Update après1995sitiframe.js

--- après1995sitiframe.js
+++ après1995sitiframe.js
@@ -1,4 +1,4 @@
-linkURL = "indexaprès1995.htm"
+linkURL = "indexaprÃ¨s1995.htm"
 	if (parent.document.URL) {
 		callingURL = parent.document.URL
 		if (callingURL.indexOf('?') != -1) {
@@ -6,9 +6,31 @@ linkURL = "indexaprès1995.htm"
 		}
 	}
 	
-Tiemkoframe = new Array ("après1995.htm", "accueil.htm","avant.htm", "1986-1995.htm", "après1995.htm","ed.htm","jjt.htm","rc.htm","Deboco.htm")
+Tiemkoframe = new Array ("", "accueil.htm","avant.htm", "1986-1995.htm", "aprÃ¨s1995.htm","ed.htm","jjt.htm","rc.htm","Deboco.htm")
 		
 function newIframe(ach) {
 	content.document.location.href = Tiemkoframe[ach]
-
 }
+
+if (document.images) {
+		Accl = new Image
+		Avnt = new Image
+		Mil = new Image
+		Apr = new Image
+
+		Accl.src = "Accueil.png"
+		Avnt.src = "Avant.png"
+		Mil.src = "1986-1995.png"
+		Apr.src = "AprÃ¨s1995.png"
+	
+		Acclx = new Image
+		Avntx = new Image
+		Milx = new Image
+		Aprx = new Image
+
+		Acclx.src = "Accueilx.png"
+		Avntx.src = "Avantx.png"
+		Milx.src = "1986-1995x.png"
+		Aprx.src = "AprÃ¨s1995x.png"
+
+	}
