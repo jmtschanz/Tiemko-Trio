97adebe947910601f9d8c0dc8339f86da8500f5d
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-16 23:37:40 +0200

    Update edsitiframe.js

--- edsitiframe.js
+++ edsitiframe.js
@@ -6,9 +6,31 @@ linkURL = "indexed.htm"
 		}
 	}
 	
-Tiemkoframe = new Array ("ed.htm", "accueil.htm","avant.htm", "1986-1995.htm", "apr�s1995.htm","ed.htm","jjt.htm","rc.htm","Deboco.htm")
+Tiemkoframe = new Array ("", "accueil.htm","avant.htm", "1986-1995.htm", "après1995.htm","ed.htm","jjt.htm","rc.htm","Deboco.htm")
 		
 function newIframe(ach) {
 	content.document.location.href = Tiemkoframe[ach]
-
 }
+
+if (document.images) {
+		Accl = new Image
+		Avnt = new Image
+		Mil = new Image
+		Apr = new Image
+
+		Accl.src = "Accueil.png"
+		Avnt.src = "Avant.png"
+		Mil.src = "1986-1995.png"
+		Apr.src = "Après1995.png"
+	
+		Acclx = new Image
+		Avntx = new Image
+		Milx = new Image
+		Aprx = new Image
+
+		Acclx.src = "Accueilx.png"
+		Avntx.src = "Avantx.png"
+		Milx.src = "1986-1995x.png"
+		Aprx.src = "Après1995x.png"
+
+	}
