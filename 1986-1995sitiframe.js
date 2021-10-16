546759c8837589b488de75d8ad16ce84fb2eaec9
jmtschanz <66422361+jmtschanz@users.noreply.github.com>
2021-10-16 16:53:28 +0200

    Add files via upload

---
+++ 1986-1995sitiframe.js
@@ -0,0 +1,14 @@
+linkURL = "index1986-1995.htm"
+	if (parent.document.URL) {
+		callingURL = parent.document.URL
+		if (callingURL.indexOf('?') != -1) {
+			linkURL = callingURL.substring(callingURL.indexOf('?')+1,callingURL.length)
+		}
+	}
+	
+Tiemkoframe = new Array ("1986-1995.htm", "accueil.htm","avant.htm", "1986-1995.htm", "après1995.htm","ed.htm","jjt.htm","rc.htm","Deboco.htm")
+		
+function newIframe(ach) {
+	content.document.location.href = Tiemkoframe[ach]
+
+}
